c1eb65f4befc07b69a395b4de7e0b4f4f872633f
Sebastian <seb8299@gmail.com>
2023-09-12 16:22:58 +0200

    try fix

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ import discord
 from discord.ext import commands
 
 message = None
-client = None
-
 # elements role ids
-water_id = 1148728509926211614
-earth_id = 1148728239578157116
-fire_id  = 1148728291893727282
-air_id   = 1148728453605097532
+# water_id = 1148728509926211614
+# earth_id = 1148728239578157116
+# fire_id  = 1148728291893727282
+# air_id   = 1148728453605097532
 
-# water_id = 1148933891701346374
-# earth_id = 1148941443247902810
-# fire_id  = 1148933929672380517
-# air_id   = 1148941410062573578
+water_id = 1148933891701346374
+earth_id = 1148941443247902810
+fire_id  = 1148933929672380517
+air_id   = 1148941410062573578
 
 intents = discord.Intents.all()
 intents.guilds = True
@@ -171,8 +169,7 @@ config = json.loads(config_file)
 token = config["token"]
 while True:
     try:
-        print("try to start client")
-        client = commands.Bot(command_prefix='/', intents=intents)                
+        print("try to start client")           
         client.run(token)
         print("client start finished")
     except Exception as e:
